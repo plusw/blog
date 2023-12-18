7e2f60ff9c40df9dec136493a6e1362396add279
plusw <a2833772178@163.com>
2023-12-18 22:57:18 +0800

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import './index.css'
+import Header from "./Header.jsx"
 import ArticleList from "./ArticleList.jsx"
 import Article from "./Article.jsx"
-import Header from "./Header.jsx"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    {/* <App /> */}
     <ArticleList />
     {/* <Article /> */}
   </React.StrictMode>,
